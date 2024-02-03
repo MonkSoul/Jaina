6f4b33f11ecfd09a8121093a95d6abb77939fd3d
百小僧 <monksoul@outlook.com>
2024-02-03 23:36:05 +0800

    发布 `v4.2.0` 版本，同步 `Furion` 所有修改

--- src/Jaina/HostedServices/EventBusHostedService.cs
+++ src/Jaina/HostedServices/EventBusHostedService.cs
@@ -1,16 +1,6 @@
-﻿// MIT 许可证
+﻿// 版权归百小僧及百签科技（广东）有限公司所有。
 //
-// 版权 © 2020-present 百小僧, 百签科技（广东）有限公司 和所有贡献者
-//
-// 特此免费授予任何获得本软件副本和相关文档文件（下称“软件”）的人不受限制地处置该软件的权利，
-// 包括不受限制地使用、复制、修改、合并、发布、分发、转授许可和/或出售该软件副本，
-// 以及再授权被配发了本软件的人如上的权利，须在下列条件下：
-//
-// 上述版权声明和本许可声明应包含在该软件的所有副本或实质成分中。
-//
-// 本软件是“如此”提供的，没有任何形式的明示或暗示的保证，包括但不限于对适销性、特定用途的适用性和不侵权的保证。
-// 在任何情况下，作者或版权持有人都不对任何索赔、损害或其他责任负责，无论这些追责来自合同、侵权或其它行为中，
-// 还是产生于、源于或有关于本软件以及本软件的使用或其它处置。
+// 此源代码遵循位于源代码树根目录中的 LICENSE 文件的许可证。
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
 using System.Logging;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace Jaina;
@@ -221,6 +212,13 @@ internal sealed class EventBusHostedService : BackgroundService
             return;
         }
 
+        // 检查是否配置只消费一次
+        if (eventSource.IsConsumOnce)
+        {
+            var randomId = RandomNumberGenerator.GetInt32(0, eventHandlersThatShouldRun.Count());
+            eventHandlersThatShouldRun = [eventHandlersThatShouldRun.ElementAt(randomId)];
+        }
+
         // 创建一个任务工厂并保证执行任务都使用当前的计划程序
         var taskFactory = new TaskFactory(System.Threading.Tasks.TaskScheduler.Current);
 
@@ -290,10 +288,10 @@ internal sealed class EventBusHostedService : BackgroundService
                 catch (Exception ex)
                 {
                     // 输出异常日志
-                    Log(LogLevel.Error, "Error occurred executing {EventId}.", new[] { eventSource.EventId }, ex);
+                    Log(LogLevel.Error, "Error occurred executing in {EventId}.", new[] { eventSource.EventId }, ex);
 
                     // 标记异常
-                    executionException = new InvalidOperationException(string.Format("Error occurred executing {0}.", eventSource.EventId), ex);
+                    executionException = new InvalidOperationException(string.Format("Error occurred executing in {0}.", eventSource.EventId), ex);
 
                     // 捕获 Task 任务异常信息并统计所有异常
                     if (UnobservedTaskException != default)
@@ -325,6 +323,7 @@ internal sealed class EventBusHostedService : BackgroundService
                     {
                         LastGCCollectTime = nowTime;
                         GC.Collect();
+                        GC.WaitForPendingFinalizers();
                     }
                 }
             }, stoppingToken);
