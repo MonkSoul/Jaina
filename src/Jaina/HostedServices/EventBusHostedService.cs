ed46be06aa5fda95eff29c2b1da93a33039cc0ff
MonkSoul <monksoul@outlook.com>
2026-06-05 14:45:45 +0800

    😊 发布 `v4.6.5` 版本，改进后台服务

--- src/Jaina/HostedServices/EventBusHostedService.cs
+++ src/Jaina/HostedServices/EventBusHostedService.cs
@@ -528,4 +528,13 @@ internal sealed class EventBusHostedService : BackgroundService
 
         await base.StopAsync(cancellationToken);
     }
+
+    /// <summary>
+    /// 释放事件订阅
+    /// </summary>
+    public override void Dispose()
+    {
+        UnobservedTaskException = null;
+        base.Dispose();
+    }
 }
